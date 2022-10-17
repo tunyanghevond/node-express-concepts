fc7bc515c64b9e7e2246f0ad8a45a43ccdd32344
Ghevond Tunyan <ghtunyan2018@gmail.com>
2022-10-17 19:58:55 +0200

    routs and controllers

---
+++ express/routes/auth.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const route = express.Router()
+
+route.post('/', (req, res) => {
+  const { name } = req.body
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`)
+  }
+
+  res.status(401).send('Please Provide Credentials')
+})
+
+module.exports = route
